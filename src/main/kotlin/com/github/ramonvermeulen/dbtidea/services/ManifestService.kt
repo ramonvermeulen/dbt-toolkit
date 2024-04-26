168073b81378c8301e52a638d4e3de057adaae28
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2024-04-26 16:46:00 +0200

    Work in progress trying to get listeners to work

--- src/main/kotlin/com/github/ramonvermeulen/dbtidea/services/ManifestService.kt
+++ src/main/kotlin/com/github/ramonvermeulen/dbtidea/services/ManifestService.kt
@@ -1,14 +1,18 @@
 package com.github.ramonvermeulen.dbtidea.services
 
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import java.io.File
 
 @Service(Service.Level.PROJECT)
-class ManifestService(var project: Project) {
+class ManifestService(private var project: Project) {
     private var interval = 5 * 60 * 1000 // 5 minutes
     private var lastUpdateTimestamp = System.currentTimeMillis() - interval // initial set now - 5 minutes
     private val dbtCommandExecutorService = project.service<DbtCommandExecutorService>()
+    private var manifest: JsonObject? = null
 
     fun parseManifest() {
         val currentTime = System.currentTimeMillis()
@@ -16,6 +20,12 @@ class ManifestService(var project: Project) {
         if (elapsedTime > interval) {
             lastUpdateTimestamp = currentTime
             dbtCommandExecutorService.executeCommand(listOf("parse"))
+        }
+        val file = File(project.basePath + "/target/manifest.json")
+        if (file.exists()) {
+            manifest = JsonParser.parseString(file.readText()).asJsonObject
+        } else {
+            println("Manifest file does not exist")
         }
     }
 }
