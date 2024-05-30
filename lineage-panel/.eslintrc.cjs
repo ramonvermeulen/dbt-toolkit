085d46fc244ebf9887eda67152b924c9ed47ec86
Ramon <ramon.vermeulen@ing.com>
2024-05-30 13:40:01 +0200

    added eslint config

--- lineage-panel/.eslintrc.cjs
+++ lineage-panel/.eslintrc.cjs
@@ -14,5 +14,15 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'sort-imports': ['error', {
+      'ignoreCase': false,
+      'ignoreDeclarationSort': false,
+      'ignoreMemberSort': false,
+      'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single'],
+      'allowSeparatedGroups': true
+    }],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always']
   },
 }
