0a428bbdc7a690181658769bb9e902186541a5d7
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2024-08-07 20:43:24 +0200

    resolve issue with fixed width for nodes

--- lineage-panel/src/nodes/DbtModelNode.tsx
+++ lineage-panel/src/nodes/DbtModelNode.tsx
@@ -1,4 +1,5 @@
 
+import { useEffect, useRef, useState } from 'react';
 import { FaChartArea } from 'react-icons/fa';
 import { FaRegNoteSticky } from 'react-icons/fa6';
 import { GoDatabase } from 'react-icons/go';
@@ -22,6 +23,22 @@ export function DbtModelNode({
     const type = parts?.[0];
     const name = parts?.[2];
     const source = parts?.[3];
+    const [isBig, setIsBig] = useState(false);
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        function checkWidth() {
+            // @ts-expect-error offsetWidth is not on any type
+            const width = containerRef.current?.offsetWidth;
+            setIsBig(width > 100);
+        }
+
+        window.addEventListener('resize', checkWidth);
+        checkWidth();
+        return () => {
+            window.removeEventListener('resize', checkWidth);
+        };
+    }, []);
 
     const renderIcon = () => {
         if (type === 'seed') {
@@ -43,13 +60,16 @@ export function DbtModelNode({
     };
 
     return (
-        <div className={`react-flow__node-default dbt-model-node ${data?.isSelected ? 'active' : ''}`}>
+        <div
+            className={`react-flow__node-default dbt-model-node ${data?.isSelected ? 'active' : ''} ${isBig ? 'width-auto' : ''}`}
+        >
             <div className="node-title">
                 <div className="icon-box">
                     <div className="icon">{renderIcon()}</div>
                     <div className="type">{type?.toUpperCase().substring(0, 3)}</div>
                 </div>
-                {name && <div className="dbt-model-title">{name}{(type == 'source') && ': ' + source}</div>}
+                {name && <div className="dbt-model-title ellipsis"
+                              ref={containerRef}>{name}{(type == 'source') && ': ' + source}</div>}
             </div>
             <Handle type="source" position={Position.Right} />
             <Handle type="target" position={Position.Left} />
