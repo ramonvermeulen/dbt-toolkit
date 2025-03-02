743a9b4ee7065a21b97e03cd256ddcbe0329d768
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-03-02 16:57:04 +0100

    feat: WIP on jinja references, still only works on IC

--- src/main/kotlin/com/github/ramonvermeulen/dbtToolkit/jinja/JinjaReferenceContributor.kt
+++ src/main/kotlin/com/github/ramonvermeulen/dbtToolkit/jinja/JinjaReferenceContributor.kt
@@ -1,17 +1,16 @@
 package com.github.ramonvermeulen.dbtToolkit.jinja
 
-import com.intellij.openapi.project.DumbAware
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiReferenceContributor
 import com.intellij.psi.PsiReferenceRegistrar
 
-class JinjaReferenceContributor : PsiReferenceContributor(), DumbAware {
+class JinjaReferenceContributor : PsiReferenceContributor() {
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
         println("Registered Jinja reference provider")
         registrar.registerReferenceProvider(
-            PlatformPatterns.psiElement(), // Match any PsiElement
-            JinjaReferenceProvider(),
-            PsiReferenceRegistrar.HIGHER_PRIORITY // Use higher priority
+            PlatformPatterns.psiElement()
+               .withText("ref"),
+            JinjaReferenceProvider()
         )
     }
 }
